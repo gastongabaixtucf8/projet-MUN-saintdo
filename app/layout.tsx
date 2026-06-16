fbcc734e042e8397b4cd46083c54a159a4e75080
Gaston mac <gastonmac@MacBook-Air-de-Stephanie.local>
2026-06-16 15:42:26 +0200

    auto-save

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export const metadata: Metadata = {
   title: 'MUN Saint Dominique — Pau, France',
