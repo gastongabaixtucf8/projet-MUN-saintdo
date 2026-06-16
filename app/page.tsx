0353bce04be82187cbdb78bc76caa226a900157b
Gaston mac <gastonmac@MacBook-Air-de-Stephanie.local>
2026-06-16 11:17:00 +0200

    auto-save

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { client } from '@/lib/sanity/client'
+
+async function getMessage(): Promise<string | null> {
+  return client.fetch(`*[_type == "siteSettings"][0].message`)
+}
+
+export default async function Page() {
+  const message = await getMessage()
+
+  return (
+    <main>
+      <h1>{message ?? 'Hello Cows'}</h1>
+    </main>
+  )
+}
