e8dc2bb35832fb5c14681c7294fd2068c3b6f057
Gaston mac <gastonmac@MacBook-Air-de-Stephanie.local>
2026-06-17 15:25:12 +0200

    auto-save

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import { galleryPhoto } from './galleryPhoto'
 import { programDocument } from './programDocument'
+import { committeeTopicsDocument } from './committeeTopicsDocument'
 
-export const schemaTypes = [galleryPhoto, programDocument]
+export const schemaTypes = [galleryPhoto, programDocument, committeeTopicsDocument]
